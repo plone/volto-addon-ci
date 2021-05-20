508c50272abe5c88f7eb4d465fb6c4e8dede7367
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-05-20 14:28:08 +0300

    add cypress coverage if babelrc_coverage file exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -92,6 +92,12 @@ if [[ "$1" == "prettier"* ]]; then
 fi
 
 if [[ "$1" == "cypress"* ]]; then
+
+  if [ - f /opt/frontend/my-volto-project/src/addons/$GIT_NAME/babelrc_coverage ]; then
+    cp /opt/frontend/my-volto-project/src/addons/$GIT_NAME/babelrc_coverage /opt/frontend/my-volto-project/.babelrc
+    yarn add -W @cypress/code-coverage
+  fi
+  
   RAZZLE_API_PATH=$RAZZLE_API_PATH yarn start &
 
   cd /opt/frontend/my-volto-project/src/addons/$GIT_NAME
