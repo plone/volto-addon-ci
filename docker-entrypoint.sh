0c323cd192920e44cc45d13c43914471a9f3d7fc
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-05-24 12:15:56 +0300

    improve coverage substitutions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -92,13 +92,11 @@ if [[ "$1" == "prettier"* ]]; then
 fi
 
 if [[ "$1" == "cypress"* ]]; then
-
+  
   if [ -f /opt/frontend/my-volto-project/src/addons/$GIT_NAME/.coverage.babel.config.js ]; then
     cp /opt/frontend/my-volto-project/src/addons/$GIT_NAME/.coverage.babel.config.js /opt/frontend/my-volto-project/babel.config.js
-    sed -i '/\/\*[ ]*coverage[ ]*start/d' /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/plugins/index.js
-    sed -i '/coverage[ ]*end[ ]*\*\//d' /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/plugins/index.js
-    sed -i '/\/\*[ ]*coverage[ ]*start/d' /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/support/index.js
-    sed -i '/coverage[ ]*end[ ]*\*\//d' /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/support/index.js
+    grep -Rl coverage-start /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/* |  xargs sed -i '/\/\*[ ]*coverage-start/d'
+    grep -Rl coverage-end /opt/frontend/my-volto-project/src/addons/$GIT_NAME/cypress/* |  xargs sed -i '/coverage-end[ ]*\*\//d'
   fi
   
   RAZZLE_API_PATH=$RAZZLE_API_PATH yarn start &
